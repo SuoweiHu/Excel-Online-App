a2047668dab6309c909a452d15fab87520c1d00c
SuoweiHu <suowei.h@gmail.com>
2021-01-25 15:39:54 +0800

    根据需求更改了新的样式，新增了办公机的启停

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-# ids=`ps -ef|grep python|grep yf|grep main.py awk '{print $2}'`
-ids=`ps -ef|grep python3|grep Excel-Online-App/main.py`
+# ids=`ps -ef|grep python|grep yf|grep main.py awk '{print $2}'`            # 22.32.170.169
+# ids=`ps -ef|grep python3|grep Excel-Online-App/main.py|awk '{print $2}'`  # 10.0.0.98
+ids=`ps -ef|grep Excel-Online-App/main.py|awk '{print $2}'`                 # local
 
 if [[ "${ids}" != "" ]]
 then 
@@ -7,6 +8,6 @@ then
 else 
     echo "[ Starting flask service at 50000 ... ]"
     # nohup python /root/yf/Excel-Online-App/main.py & 2>1&
-    # nohup python3 /Users/suoweihu/Documents/GitHub/Excel-Online-App/main.py
-    nohup python3 ~/Excel-Online-App/main.py
+    nohup python3 /Users/suoweihu/Documents/GitHub/Excel-Online-App/main.py
+    # nohup python3 ~/Excel-Online-App/main.py
 fi 
--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
-# ids=`ps -ef|grep python|grep yf|grep main.py awk '{print $2}'`
-ids=`ps -ef|grep python3|grep Excel-Online-App/main.py`
+# ids=`ps -ef|grep python|grep yf|grep main.py awk '{print $2}'`            # 22.32.170.169
+# ids=`ps -ef|grep python3|grep Excel-Online-App/main.py|awk '{print $2}'`  # 10.0.0.98
+ids=`ps -ef|grep Excel-Online-App/main.py|awk '{print $2}'`                 # local
 
 if [[ "${ids}" = "" ]]
 then 
