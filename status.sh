bfd6c82eef052e4bc703e13282ac0caa31fa638c
SuoweiHu <suowei.h@gmail.com>
2021-01-27 08:36:29 +0800

    完成了用户编辑页全量导出（通过多次请求返回所有分页数据）
    Ref https://blog.csdn.net/weixin_41619400/article/details/103961645

--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
 # ps -ef|grep python|grep yf|grep main.py
-ps -ef|grep python3|grep Excel-Online-App/main.py
+ps -ef | grep main.py | grep -v grep
 echo
 netstat -an|grep 5000
--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 # ids=`ps -ef|grep python|grep yf|grep main.py awk '{print $2}'`            # 22.32.170.169
 # ids=`ps -ef|grep python3|grep Excel-Online-App/main.py|awk '{print $2}'`  # 10.0.0.98
-ids=`ps -ef|grep Excel-Online-App/main.py|awk '{print $2}'`                 # local
+ids=`ps -ef | grep main.py | grep -v grep |awk '{print $2}'`    # local
+
 
 if [[ "${ids}" = "" ]]
 then 
