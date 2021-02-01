dfa5cf38eca2d4ccf690ea3724f982b664a97177
SuoweiHu <suowei.h@gmail.com>
2021-02-01 11:02:43 +0800

    更改了获取表格的异步数据接口从 /data/<string:tb_name> 为 /api/data?tb_name=...

--- routes_table_dynamic.py
+++ routes_table_dynamic.py
@@ -34,8 +34,9 @@ from routes_table_static    import *
 
 # =============================================
 # 自动渲染表格 - 主界面数据接口
-# @app.route('')
-# def 
+# @app.route('/api/data/')
+# def apiData_dataMain():
+    # return
 
 
 # =============================================
@@ -57,100 +58,107 @@ def upload_file():
         return render_template("redirect_fileUploaded.html",\
             message=f"上传文件失败, 错误: 位置上传方法 (需要为POST)")
 
-@app.route('/data_all/<string:table_name>')
-def export_table_data_all(table_name):
+# DEPRECATED
+# @app.route('/data_all/<string:table_name>')
+# def export_table_data_all(table_name):
+    # """
+    # 数据接口
+    # """
+    # show_operator        = True   # 是否展示操作员信息
+    # # authorization_inedx  = 0    # 用于确认用户权限的列的序号 (这里 0 指的是 ‘行号’ 在标题的第一个位置)
+    # # authorized_rows      = [733101,733121,733131,733141,733151,733165,733177,733189,733201,733213,733225]
+    # authorized_rows      = Database_Utils.user.get_rows(session['operator_name'])
+    # authorized_rows      = [str(i) for i in authorized_rows]
+
+    # # ========================================
+    # # Layui数据接口 默认返回属性
+    # code  = 0  # 0 means success
+    # msg   = ""
+    # count = 0
+    # data = []
+
+    # sort = (None,None)
+    # if('field' in dict(request.args).keys()):
+    #     key   =  request.args.get('field')
+    #     if(key == "操作员"):    key = 'user.name'
+    #     elif(key == "时间"):    key = 'user.time' 
+    #     else:                  key = 'data.' + key
+    #     order = (-1) if(request.args.get('order') == 'desc') else (1)
+    #     sort  = (key, order)
+    # else:
+    #     sort = (None, None) 
+
+    # app.logger.debug(f"\t\t重新获得信息")
+    # app.logger.debug(f"\t\t排序信息:{sort}")
+
+    # # 从数据库读取对应表格
+    # role  = ("管理员") if Database_Utils.user.check_admin(session['operator_name']) else ("普通用户")
+    # timer = debugTimer(f"开始获取表格 (用户:{session['operator_name']}, 权限: {role})", "获取表格完毕")
+    # timer.start()
+    # # 生成查询问句
+    # if(Database_Utils.user.check_admin(session['operator_name'])):row_query = {}
+    # else:row_query = {'data.行号': {'$in': authorized_rows}}
+    # # 尝试获取数据（如果不存在数据则会抛出RuntimeError错误）
+    # try: 
+    #     table_data = Database_Utils.table.load_table(tb_name=table_name, search_query=row_query, sort=sort)
+    #     print(table_data.toJson)
+    # except RuntimeError as e: 
+    #     app.logger.debug(f"\t\t{e}")
+    #     emptyTable_json = dict({})
+    #     table_data = TableData(json=emptyTable_json, tb_name=table_show)
+    # timer.end()
+
+    # # 处理获得的文档成为可被Juinja2渲染的格式
+    # for i in range(len(table_data.rows)):
+    #     # if(table_data.rows[i][authorization_inedx] in authorized_rows) or (Database_Utils.user.check_admin(session['operator_name'])):
+    #     temp = {}
+    #     # UUID
+    #     _id  = table_data.ids[i]
+    #     temp['_id'] = _id
+    #     # 数据
+    #     _row = table_data.rows[i]
+    #     for j in range(len(table_data.titles)):
+    #             _cell  =_row[j]
+    #             _title =  table_data.titles[j]
+    #             temp[_title] = _cell
+    #     # 操作员
+    #     if(show_operator):
+    #         for j in range(len(table_data.operator_titles)):
+    #             op_title =  table_data.operator_titles[j]
+    #             op_data  =  table_data.operators[i][j]
+    #             temp[op_title] = op_data
+    #     # 添加当前行
+    #     data.append(temp)
+
+    # count = len(data)
+    # return {"code":code, "msg":msg, "count":count, "data":data}
+
+@app.route('/api/data')
+def apiData_dataEdit(): 
     """
     数据接口
     """
+    # 获取 表格信息
+    table_name = request.args['tb_name']
+
+    # 获取 操作员信息
     show_operator        = True   # 是否展示操作员信息
     # authorization_inedx  = 0    # 用于确认用户权限的列的序号 (这里 0 指的是 ‘行号’ 在标题的第一个位置)
     # authorized_rows      = [733101,733121,733131,733141,733151,733165,733177,733189,733201,733213,733225]
     authorized_rows      = Database_Utils.user.get_rows(session['operator_name'])
     authorized_rows      = [str(i) for i in authorized_rows]
 
-    # ========================================
-    # Layui数据接口 默认返回属性
-    code  = 0  # 0 means success
-    msg   = ""
-    count = 0
-    data = []
-
-    sort = (None,None)
-    if('field' in dict(request.args).keys()):
-        key   =  request.args.get('field')
-        if(key == "操作员"):    key = 'user.name'
-        elif(key == "时间"):    key = 'user.time' 
-        else:                  key = 'data.' + key
-        order = (-1) if(request.args.get('order') == 'desc') else (1)
-        sort  = (key, order)
-    else:
-        sort = (None, None) 
-
-    app.logger.debug(f"\t\t重新获得信息")
-    app.logger.debug(f"\t\t排序信息:{sort}")
-
-    # 从数据库读取对应表格
-    role  = ("管理员") if Database_Utils.user.check_admin(session['operator_name']) else ("普通用户")
-    timer = debugTimer(f"开始获取表格 (用户:{session['operator_name']}, 权限: {role})", "获取表格完毕")
-    timer.start()
-    # 生成查询问句
-    if(Database_Utils.user.check_admin(session['operator_name'])):row_query = {}
-    else:row_query = {'data.行号': {'$in': authorized_rows}}
-    # 尝试获取数据（如果不存在数据则会抛出RuntimeError错误）
-    try: 
-        table_data = Database_Utils.table.load_table(tb_name=table_name, search_query=row_query, sort=sort)
-        print(table_data.toJson)
-    except RuntimeError as e: 
-        app.logger.debug(f"\t\t{e}")
-        emptyTable_json = dict({})
-        table_data = TableData(json=emptyTable_json, tb_name=table_show)
-    timer.end()
-
-    # 处理获得的文档成为可被Juinja2渲染的格式
-    for i in range(len(table_data.rows)):
-        # if(table_data.rows[i][authorization_inedx] in authorized_rows) or (Database_Utils.user.check_admin(session['operator_name'])):
-        temp = {}
-        # UUID
-        _id  = table_data.ids[i]
-        temp['_id'] = _id
-        # 数据
-        _row = table_data.rows[i]
-        for j in range(len(table_data.titles)):
-                _cell  =_row[j]
-                _title =  table_data.titles[j]
-                temp[_title] = _cell
-        # 操作员
-        if(show_operator):
-            for j in range(len(table_data.operator_titles)):
-                op_title =  table_data.operator_titles[j]
-                op_data  =  table_data.operators[i][j]
-                temp[op_title] = op_data
-        # 添加当前行
-        data.append(temp)
-
-    count = len(data)
-    return {"code":code, "msg":msg, "count":count, "data":data}
-
-@app.route('/data/<string:table_name>')
-def export_table_data(table_name): 
-    """
-    数据接口
-    """
-    show_operator        = True   # 是否展示操作员信息
-    # authorization_inedx  = 0    # 用于确认用户权限的列的序号 (这里 0 指的是 ‘行号’ 在标题的第一个位置)
-    # authorized_rows      = [733101,733121,733131,733141,733151,733165,733177,733189,733201,733213,733225]
-    authorized_rows      = Database_Utils.user.get_rows(session['operator_name'])
-    authorized_rows      = [str(i) for i in authorized_rows]
-
-    # 分页请求
+    # 获取 分页请求
     page  = int(request.args['page'])
     limit = int(request.args['limit'])
     start = (page - 1) * limit
     end   = start + limit
 
     # ========================================
+    # 返回 获取到的数据
+
     # Layui数据接口 默认返回属性
-    code  = 0  # 0 means success
+    code  = 0  # 0 means success 
     msg   = ""
     count = 0
     data = []
@@ -214,9 +222,11 @@ def export_table_data(table_name):
     data = data[start:end]
     return {"code":code, "msg":msg, "count":count, "data":data}
 
-# =============================================
-# 自动渲染表格 - 展示表格/提交变更
 
+# =============================================
+# 自动渲染表格 - 展示表格/提交变更 
+
+# 表格展示
 @app.route('/edit/<string:table_name>')
 def edit_specified_table(table_name):
 
@@ -226,11 +236,9 @@ def edit_specified_table(table_name):
     # ========================================
     # 设置行属性 
     column_titles = Database_Utils.table.get_tableTitles(table_name) 
-    count_authRows = len(export_table_data_all(table_name=table_name)['data'])
-
-    # is_admin       = Database_Utils.user.check_admin(session['operator_name'])
-    # auth_rows      = Database_Utils.user.get_rows(session['operator_name'])
-    # count_authRows = Database_Utils.stat.count_allRows(tb_name=table_name, is_admin=is_admin)
+    is_admin       = Database_Utils.user.check_admin(session['operator_name'])
+    auth_rows      = Database_Utils.user.get_rows(session['operator_name'])
+    count_authRows = Database_Utils.stat.count_allRows(tb_name=table_name, is_admin=is_admin, authorized_banknos=auth_rows)
 
     if(show_operator): column_titles += TableData.operator_titles
     # (这里的id:title映射将被Jinja2赋值到 layui数据表单中的 每行的field上)
@@ -260,6 +268,7 @@ def edit_specified_table(table_name):
         first_entry         = first_entry
         )
 
+# 提交普通单元格
 @app.route('/submit',methods=["POST"])
 def submit_specified_table():
     """
@@ -303,6 +312,7 @@ def submit_specified_table():
 
     return "Success !"
 
+# 点击多选单元格跳出的选项
 @app.route('/multiChoice/<string:tb_name>/<string:title>/<string:_id>')
 def edit_multiChoice(tb_name,title, _id):
     meta = Database_Utils.meta.load_tablemMeta(tb_name=tb_name)
@@ -316,6 +326,7 @@ def edit_multiChoice(tb_name,title, _id):
         submit_url  = url_for('submit_mulitiChoise')
     )
 
+# 提交多选单元格选项
 @app.route('/submit_multiChoice')
 def submit_mulitiChoise():
     op_name = session['operator_name']
